c5d27b48d5030c1939ffddbce8ecf29c255293d2
Anton Sorokey <ansorokey@gmail.com>
2025-03-11 22:37:32 -0400

    monster table planning

--- db/database.js
+++ db/database.js
@@ -1,5 +1,44 @@
-const data = {
-    "Large Monsters": {},
-    "Endemic Life": {},
-    "Aquatic Life": {},
+const chatacabra = {
+    name: 'Chatacabra',
+    type: 'Amphibian',
+    habitats: [
+        'Plains'
+    ],
+    characteristics: "",
+    specialAttacks: [],
+    helpfulHints: "",
+    hide: [
+        {
+            bodyPart: 'Head',
+            effectiveness: [
+                {
+                    type: 'Blunt',
+                    level: '4'
+                }
+            ]
+        }
+    ],
+    recommendedElements: ['Ice', 'Thunder'],
+    effectiveStatus: [
+        {
+            status: 'Poison',
+            level: 3
+        }
+    ],
+    effectiveItems: [
+        {
+            item: 'Flash Pod',
+            isEffective: True
+        }
+    ],
+    materials: [
+        {
+            name: 'Catacabra Shell',
+            frequency: 5,
+            description: '',
+            rarity: 6,
+            value: 1700
+        }
+    ],
+    titles: []
 }
